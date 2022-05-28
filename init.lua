2e720deda4af6f428a5462d9f99fcd9b1054a883
garasu <nickko@yeah.net>
2022-05-29 01:07:40 +0800

    update telescope keymap

--- init.lua
+++ init.lua
@@ -11,8 +11,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 -- keymaps
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', ',f', '<cmd>Telescope fd<cr>', opts)
-vim.api.nvim_set_keymap('n', ',b', '<cmd>Telescope buffers<cr>', opts)
+vim.api.nvim_set_keymap('n', ',f', [[<cmd>lua require('telescope.builtin').find_files()<cr>]], opts)
+vim.api.nvim_set_keymap('n', ',g', [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], opts)
+vim.api.nvim_set_keymap('n', ',b', [[<cmd>lua require('telescope.builtin').buffers()<cr>]], opts)
 vim.api.nvim_set_keymap('n', ',t', [[<cmd>lua require('plugins.telescope').tabs()<cr>]], opts)
 -- copy
 vim.api.nvim_set_keymap('v', '<D-c>', [["+y]], opts)
