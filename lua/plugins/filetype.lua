490f13345a49500b247112968c31645fb89fd238
garasu <nickko@yeah.net>
2022-06-07 17:48:15 +0800

    treesitter parser can not function normally when filetype is on

--- lua/plugins/filetype.lua
+++ lua/plugins/filetype.lua
@@ -1,12 +1,13 @@
 require('filetype').setup({
     overrides = {
         function_extensions = {
-            ['go'] = function()
-                -- vim.bo.filetype = 'go'
-                -- Remove annoying indent jumping
-                vim.bo.expandtab = false
-                vim.bo.cinoptions = vim.bo.cinoptions .. 'L0'
-            end
+            -- ['lua'] = function() end,
+            -- ['go'] = function()
+            --     -- vim.bo.filetype = 'go'
+            --     -- Remove annoying indent jumping
+            --     vim.bo.expandtab = false
+            --     vim.bo.cinoptions = vim.bo.cinoptions .. 'L0'
+            -- end
         }
     }
 })
