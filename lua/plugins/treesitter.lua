58e0751ea818e64414619a1b0ff8913dd57971f2
garasu <nickko@yeah.net>
2022-07-20 19:07:33 +0800

    use custom color in rainbow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+local colors = require 'tools'.colors
 require 'nvim-treesitter.configs'.setup {
     ensure_installed = { 'lua', 'vim' },
     highlight = {
@@ -6,6 +7,15 @@ require 'nvim-treesitter.configs'.setup {
     },
     rainbow = {
         enable = true,
+        colors = {
+            colors.yellow,
+            colors.cyan,
+            colors.blue,
+            colors.green,
+            colors.orange,
+            colors.violet,
+            colors.magenta,
+        },
         extended_mode = true,
         max_file_lines = nil,
     }
