e4b9209f474243f3f4c8791e50f5e41ff7f8180c
garasu <nickko@yeah.net>
2022-07-20 19:26:46 +0800

    cancel diagnostic sign column

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,16 +5,22 @@ local common_attach_func = function(client)
     require 'lsp-format'.on_attach(client)
 end
 
+-- cancel signcolumns on purpose
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    -- vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    vim.fn.sign_define(hl, { text = '', texthl = '', numhl = '' })
+end
+
 -- lua
 local luadev = require 'lua-dev'.setup {
-    -- add any options here, or leave empty to use the default settings
     lspconfig = {
         on_attach = function(client)
             common_attach_func(client)
         end,
     },
 }
-
 lspconfig.sumneko_lua.setup(luadev)
 
 -- golang
