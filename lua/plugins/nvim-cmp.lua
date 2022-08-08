8b14b257b3391db70529307488c676aeb41961f5
garasu <nickko@yeah.net>
2022-08-08 19:05:41 +0800

    add ultisnips configuration and some go snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -28,6 +28,19 @@ local kind_icons = {
     TypeParameter = ''
 }
 
+vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
+vim.g.UltiSnipsJumpForwardTrigger = '<Tab>'
+vim.g.UltiSnipsJumpBackwardTrigger = '<S-Tab>'
+vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
+vim.g.UltiSnipsRemoveSelectModeMappings = 0
+
+vim.g.UltiSnipsSnippetDirectories = { "UltiSnips" }
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+
 cmp.setup({
     preselect = cmp.PreselectMode.None,
     snippet = {
@@ -47,6 +60,19 @@ cmp.setup({
         ['<C-f>']     = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-c>']     = cmp.mapping.abort(),
+        ['<Tab>']     = cmp.mapping(
+            function(fallback)
+                if cmp.visible() then
+                    cmp.mapping.close()
+                    if vim.fn['UltiSnips#CanExpandSnippet']() == 1 then
+                        vim.api.nvim_feedkeys(t("<Plug>(ultisnips_expand)"), 'm', true)
+                    end
+                else
+                    fallback()
+                end
+            end,
+            --{ i = cmp.mapping.close(), c = cmp.mapping.close() }),
+            { 'i', 'c' }),
         ['<CR>']      = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources(
